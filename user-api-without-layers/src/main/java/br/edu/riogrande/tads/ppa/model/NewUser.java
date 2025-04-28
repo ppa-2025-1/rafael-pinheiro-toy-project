ea9fc02c480db68d1bcdcce14f9952d14b82fa93
Rafael Pinheiro <Rafaelmessipinheiro@gmail.com>
2025-04-28 19:31:21 -0300

    Adiciona validação ao modelo NewUser e implementa UserService para lógica de criação de usuários

--- user-api-without-layers/src/main/java/br/edu/riogrande/tads/ppa/model/NewUser.java
+++ user-api-without-layers/src/main/java/br/edu/riogrande/tads/ppa/model/NewUser.java
@@ -2,11 +2,24 @@ package br.edu.riogrande.tads.ppa.model;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record NewUser(
+        @NotNull(message = "Nome não pode ser vazio") 
         String name,
+
         String handle,
+
+        @NotNull(message = "Email não pode ser vazio") 
+        @Email(message = "Email inválido") 
         String email,
+
+        @NotNull(message = "Senha não pode ser vazia") 
+        @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres") 
         String password,
+
         String company,
         Profile.AccountType type,
         List<String> roles
